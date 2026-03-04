e4f0edfde43c18e859ba0a49ae1825528bc4ba1a
dteta2gh <Dteta2@gmail.com>
2026-03-04 10:38:41 -0500

    Create chess_config.php with game configuration

---
+++ config/chess_config.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'time_controls' => [
+        'blitz' => '3+2',  // 3 minutes + 2 seconds increment
+        'rapid' => '10+5', // 10 minutes + 5 seconds increment
+        'classical' => '60+30', // 60 minutes + 30 seconds increment
+    ],
+    'game_modes' => [
+        'single_player',
+        'multiplayer',
+        'tournament',
+    ],
+    'ai_settings' => [
+        'difficulty' => [
+            'easy',
+            'medium',
+            'hard',
+        ],
+    ],
+];
